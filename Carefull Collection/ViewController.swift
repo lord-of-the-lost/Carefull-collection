2fd9228ab67ac3a345b99287ec00c80de0796812
Nikolay Ignatov <ignatov.99@yandex.ru>
2023-07-27 17:51:12 +0300

    add collectionView

--- Carefull Collection/ViewController.swift
+++ Carefull Collection/ViewController.swift
@@ -7,13 +7,77 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    private let cellWidth: CGFloat = 300
+    private let cellHeight: CGFloat = 500
+    private let cellSpacing: CGFloat = 10
+    
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.sectionInset.left = 20
+        layout.itemSize = CGSize(width: cellWidth, height: cellHeight)
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.backgroundColor = .white
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupNavigationBar()
+        setupView()
+        setupConstraints()
     }
-
-
+    
+    private func setupNavigationBar() {
+        title = "Collection"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    private func setupView() {
+        view.backgroundColor = .white
+        view.addSubview(collectionView)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
 }
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        cell.backgroundColor = .systemGray
+        cell.layer.cornerRadius = 10
+        
+        return cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        let itemWidthWithSpacing = cellWidth + cellSpacing
+        
+        let currentItem = Int(targetContentOffset.pointee.x / itemWidthWithSpacing)
+        
+        let newHorizontalOffset = CGFloat(currentItem) * itemWidthWithSpacing
+        
+        targetContentOffset.pointee = CGPoint(x: newHorizontalOffset, y: 0)
+    }
+}
