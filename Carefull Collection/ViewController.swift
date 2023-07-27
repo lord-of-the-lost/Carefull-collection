76544c81c29ed19539d208116141db2802437e0f
Nikolay Ignatov <ignatov.99@yandex.ru>
2023-07-27 15:19:31 +0300

    Initial Commit

---
+++ Carefull Collection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Carefull Collection
+//
+//  Created by Николай Игнатов on 27.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
